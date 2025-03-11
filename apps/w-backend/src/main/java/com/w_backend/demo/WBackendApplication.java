2c75f1cda36e77c9f7846c3e223e5036fb7dbf4a
Diego Ruiz Muñoz <runyxp@gmail.com>
2025-03-11 22:13:39 +0100

    [W] add config for run

--- apps/w-backend/src/main/java/com/w_backend/demo/WBackendApplication.java
+++ apps/w-backend/src/main/java/com/w_backend/demo/WBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WBackendApplication {
+	public static final String DEV_PROFILE = "dev";
 
 	public static void main(String[] args) {
 		SpringApplication.run(WBackendApplication.class, args);
