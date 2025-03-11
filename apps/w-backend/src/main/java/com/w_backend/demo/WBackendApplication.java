d242c5ba9c9fb6f02d2ce10ceb816636cb8e6676
Diego Ruiz Muñoz <runyxp@gmail.com>
2025-03-11 16:05:14 +0100

    [W] add backend java

---
+++ apps/w-backend/src/main/java/com/w_backend/demo/WBackendApplication.java
@@ -0,0 +1,15 @@
+package com.w_backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WBackendApplication {
+
+	public static void main(String[] args) {
+	//System.out.println("Hello World");
+		SpringApplication.run(WBackendApplication.class, args);
+
+	}
+
+}
