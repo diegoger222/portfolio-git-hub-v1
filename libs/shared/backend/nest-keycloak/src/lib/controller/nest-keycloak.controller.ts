5c55164dfb3135c1fd962fabce3536c7a8de4f31
Diego Ruiz Muñoz <runyxp@gmail.com>
2025-04-23 10:12:41 +0200

    ADD auth EndPoints

--- libs/shared/backend/nest-keycloak/src/lib/controller/nest-keycloak.controller.ts
+++ libs/shared/backend/nest-keycloak/src/lib/controller/nest-keycloak.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { NestKeycloakService } from '../services/nest-keycloak.service';
 import { LoginRequestBody } from '@portfolio-git-hub-v1/ts-interfaces';
+import { Unprotected } from 'nest-keycloak-connect';
 
 @Controller('auth')
 export class NestKeycloakController {
   constructor(private readonly keycloakService: NestKeycloakService) {}
 
-  @Get('login')
+  @Post('login')
+  @Unprotected()
   login(@Body() loginDetails: LoginRequestBody) {
     return this.keycloakService.login(loginDetails.email, loginDetails.password);
+  }
+
+  @Post('refresh-token')
+  @Unprotected()
+  refreshToken(@Body('refreshToken') refreshToken: string) {
+    return this.keycloakService.refreshToken(refreshToken);
+  }
+
+  @Post('logout')
+  logOut(@Body('refreshToken') refreshToken: string) {
+    return this.keycloakService.logout(refreshToken);
   }
 }
