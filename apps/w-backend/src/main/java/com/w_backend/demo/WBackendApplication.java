1026c38b1a2834bfeccd21c24b35833766e96683
Diego Ruiz Muñoz <runyxp@gmail.com>
2025-06-06 23:31:00 +0200

    [w-backend] add: - init dev-test- config - init kafka container - init docker backend

--- apps/w-backend/src/main/java/com/w_backend/demo/WBackendApplication.java
+++ apps/w-backend/src/main/java/com/w_backend/demo/WBackendApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WBackendApplication {
 	public static final String DEV_PROFILE = "dev";
+	public static final String PROD_PROFILE = "prod";
+	public static final String TEST_PROFILE = "test";
+	public static final String DEFAULT_PROFILE = DEV_PROFILE;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WBackendApplication.class, args);
