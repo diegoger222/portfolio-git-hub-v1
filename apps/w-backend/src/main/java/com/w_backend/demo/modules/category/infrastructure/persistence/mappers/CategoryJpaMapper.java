826dac11e6a2caebe81a92fbfba778ce24b31fb7
Diego Ruiz Muñoz <runyxp@gmail.com>
2025-06-08 13:07:35 +0200

    [w-backend] update flyway with sql

--- apps/w-backend/src/main/java/com/w_backend/demo/modules/category/infrastructure/persistence/mappers/CategoryJpaMapper.java
+++ apps/w-backend/src/main/java/com/w_backend/demo/modules/category/infrastructure/persistence/mappers/CategoryJpaMapper.java
@@ -1,6 +1,7 @@
 package com.w_backend.demo.modules.category.infrastructure.persistence.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.w_backend.demo.modules.category.domain.models.Category;
@@ -12,6 +13,7 @@ public interface CategoryJpaMapper {
         CategoryJpaMapper INSTANCE = Mappers.getMapper(CategoryJpaMapper.class);
 
         // Map the domain model to the JPA entity
+        @Mapping(target = "createdAt", ignore = true) // createdAt will be set automatically
         CategoryJpaEntity domainModelToCategoryJpaEntity(Category category);
 
         // Map the JPA entity to the domain model
