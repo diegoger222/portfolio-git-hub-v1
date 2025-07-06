1b30939fb64a7eb7d1a39530a8dca6730f88cb25
Diego Ruiz Muñoz <runyxp@gmail.com>
2025-07-06 19:44:03 +0200

    fix backend kafka profile

--- apps/w-backend/src/main/java/com/w_backend/demo/common/config/kafka/KafkaTopic.java
+++ apps/w-backend/src/main/java/com/w_backend/demo/common/config/kafka/KafkaTopic.java
@@ -3,9 +3,11 @@ package com.w_backend.demo.common.config.kafka;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.config.TopicBuilder;
 import java.util.logging.Logger;
 
+@Profile({ "!test" })
 @Configuration
 public class KafkaTopic {
 
